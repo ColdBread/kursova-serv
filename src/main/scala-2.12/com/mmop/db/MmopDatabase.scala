822f413aa89c02fcdf5ca699f53e81b680e8872a
Symonenko Dmytro <sdimka0777@gmail.com>
2018-03-26 19:32:52 +0300

    Driver class chenged

--- src/main/scala-2.12/com/mmop/db/MmopDatabase.scala
+++ src/main/scala-2.12/com/mmop/db/MmopDatabase.scala
@@ -3,7 +3,7 @@ package com.mmop.db
 import scalikejdbc.{ConnectionPool, DB, DBSession, using}
 
 object MmopDatabase {
-  Class.forName("com.mysql.jdbc.Driver")
+  Class.forName("com.mysql.cj.jdbc.Driver")
   ConnectionPool.add("mmop", "jdbc:mysql://kursova-mysql.clgpufg6ovc7.us-east-2.rds.amazonaws.com/kursova", "admin", "1qa2ws3ed")
   implicit val connection = ConnectionPool.get("mmop")
 
